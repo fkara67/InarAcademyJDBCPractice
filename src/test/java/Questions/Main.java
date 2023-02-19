f7e7eb4fdca82943a08a05cd53764733ee7621b3
Fatih Kara <fkara1667@gmail.com>
2023-02-19 21:00:24 +0300

    homework

--- src/test/java/Questions/Main.java
+++ src/test/java/Questions/Main.java
@@ -4,6 +4,6 @@ import java.sql.*;
 public class Main {
     public static void main(String[] args) throws SQLException {
         //Question.answer();
-        Question2.answer();
+        Question3.answer();
     }
 }
